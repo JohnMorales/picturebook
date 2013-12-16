1307b7d64a30361815fc783059f6b46de08585f6
JohnMorales <jmorales@gmail.com>
2013-12-15 23:02:40 -0500

    fixed test

--- spec/picture_spec.rb
+++ spec/picture_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe "Picture" do
   it "Will store file path" do
+    picture_count = Picture.all.count
     p = Picture.new
     p.file_path = "/Users/fake_user/image.jpg"
     p.save!
-    Picture.all.count.should eql(1)
+    Picture.all.count.should eql(picture_count + 1)
   end
 end
